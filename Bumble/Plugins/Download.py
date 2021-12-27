65755d01d0dc65acd4dd843e6a61a5aa6cbc2edd
Optimus  ᴛʜᴇ Pʀɪᴍᴇ <95513988+Optimus-Projects@users.noreply.github.com>
2021-12-27 09:36:25 +0530

    Update Download.py

--- Bumble/Plugins/Download.py
+++ Bumble/Plugins/Download.py
@@ -11,9 +11,9 @@ from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
                             InputMediaDocument, InputMediaVideo, Message)
 from youtubesearchpython import VideosSearch
 
-from Yukki import MUSIC_BOT_NAME, app
-from Yukki.Utilities.changers import time_to_seconds
-from Yukki.Utilities.download import get_formats, get_type
+from Bumble import MUSIC_BOT_NAME, app
+from Bumble.Utilities.changers import time_to_seconds
+from Bumble.Utilities.download import get_formats, get_type
 
 user_time = {}
 flex = {}
